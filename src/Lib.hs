dfed95325786d884fc2d76e8c8832f3fc193a8c2
Weiqifan1 <cmlykke@hotmail.com>
2021-04-09 20:47:52 +0200

    codewars Array.diff 6 kyu

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( someFunc, myFunc
+    ( someFunc, myFunc, remove, exist, difference
     ) where
 
 someFunc :: IO ()
@@ -7,3 +7,24 @@ someFunc = putStrLn "someFunc"
 
 myFunc :: String
 myFunc = "myFunc"
+
+difference :: Eq a => [a] -> [a] -> [a]
+difference (x:xs) [] = (x:xs)
+difference [] _ = []
+difference (x:xs) (y:ys) | (exist y (x:xs)) == True = difference (remove y (x:xs)) ys
+                         | (exist y (x:xs)) == False = difference (x:xs) ys
+
+exist :: Eq a => a -> [a] -> Bool
+exist _ [] = False
+exist a (x : xs) | a == x = True
+                 | a /= x = exist a xs
+
+remove :: Eq a => a -> [a] -> [a]
+remove _ [] = []
+remove x (y:ys) | x == y = remove x ys
+                | otherwise = y : remove x ys
+
+
+
+
+-- slut
