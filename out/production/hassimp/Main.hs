6b032b39571ea726774d7c39aa090cceb3373e0b
Weiqifan1 <cmlykke@hotmail.com>
2021-04-10 11:14:18 +0200

    chinese lookup project - can output a textfile

--- out/production/hassimp/Main.hs
+++ out/production/hassimp/Main.hs
@@ -4,6 +4,8 @@ import Lib
 import Data.Char
 import Data.List.Split
 import Data.List
+import Control.Monad
+import System.IO
 -- regex tdfa
 -- https://williamyaoh.com/posts/2019-04-11-cheatsheet-to-regexes-in-haskell.html
 -- https://hackage.haskell.org/package/regex-tdfa
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,29 @@
 module Lib
-    ( someFunc, myFunc
+    ( someFunc, myFunc, readzz
     ) where
 
 import Data.List.Split
 import Data.List
+import Control.Monad
 -- regex tdfa
 -- https://williamyaoh.com/posts/2019-04-11-cheatsheet-to-regexes-in-haskell.html
 -- https://hackage.haskell.org/package/regex-tdfa
 import Text.Regex.TDFA
 import Text.Regex.TDFA.Text ()
-  
+import System.IO
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 myFunc :: String
 myFunc = "myFunc"
+
+readzz :: IO String
+readzz = do
+        let list = []
+        handle <- openFile "src/resources/zz201906_test.txt" ReadMode
+        contents <- hGetContents handle
+        return contents
 
 
 
